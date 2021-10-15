436690a93c1e8b4a41f323cf88d5bb5f5d951052
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2021-10-15 13:26:05 -0400

    Create Reflect.py

---
+++ Reflect.py
@@ -0,0 +1,31 @@
+def reflect():
+    X = []
+    Y = []
+    YRange = 0
+    index = 0
+    XRange = 0
+    CordNum = int(input("Enter Number on Cords:"))
+    for i in range(CordNum):
+        CordsX = int(input("Enter X:"))
+        X.append(CordsX)
+        CordsY = int(input("Enter Y:"))
+        Y.append(CordsY)
+    mirrorAxis = input("Enter Reflection Axis:").lower()
+    if mirrorAxis == "x":
+        for item in Y:
+            Y[YRange] *= -1
+            YRange += 1
+        YRange = 0
+            
+    elif mirrorAxis == "y":
+        for item in X:
+            X[XRange] *= -1
+            XRange += 1
+        XRange = 0
+    else:
+        print("please enter 'x' or 'y'")
+        
+    for item in X:
+        print(f'{index}({X[index]},{Y[index]})')
+        index += 1
+reflect()
