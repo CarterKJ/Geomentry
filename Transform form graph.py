5ec0bfb861fd2a0faeb425906208516846cdca94
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2021-10-15 13:24:24 -0400

    Create Transform form graph.py

---
+++ Transform form graph.py
@@ -0,0 +1,7 @@
+def transDif():
+    x1 = int(input("Enter X1:"))
+    y1 = int(input("Enter y1:"))
+    x2 = int(input("Enter X2:"))
+    y2 = int(input("Enter y2:"))
+    print(f'X = {x2 - x1} Y = {y2 - y1}')
+transDif()
