2b39592bc6bf12470c3c6d973153191c1a92d5b5
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2021-10-20 10:07:09 -0400

    Create Rotate.py

---
+++ Geomentry/Chapter 4 Transformations/Section 4.3 - Rotations/Rotate.py
@@ -0,0 +1,129 @@
+def rotate():
+    X = []
+    Y = []
+    Xhold=[]
+    Yhold=[]
+    index = 0
+    CordNum = int(input("Enter Number of Cords:"))
+    for i in range(CordNum):
+        CordsX = int(input("Enter X:"))
+        X.append(CordsX)
+        CordsY = int(input("Enter Y:"))
+        Y.append(CordsY)
+    Direction = input("Enter C or CC:").lower()
+    if Direction == "c":
+        deg = input("Enter Rotation ('90','180','270')").lower()
+        if deg == "90":
+            for i in X:
+                X[index] *= -1
+                Xhold.append(X[index])
+                index+=1
+            X.clear()
+            index = 0
+            for i in Y:
+                Yhold.append(Y[index])
+                index += 1
+            Y.clear()
+            index = 0
+            for i in Yhold:
+                X.append(Yhold[index])
+                Y.append(Xhold[index])
+                index += 1
+            Xhold.clear()
+            Yhold.clear()
+            index += 0
+        if deg == "180":
+            for i in X:
+                X[index] *= -1 
+                Y[index] *= -1
+                index += 1 
+        index = 0 
+        
+        if deg == "270":
+            for i in X:
+                Xhold.append(X[index])
+                index+=1
+            X.clear()
+            index = 0
+            for i in Y:
+                Y[index] *= -1
+                Yhold.append(Y[index])
+                index += 1
+            Y.clear()
+            index = 0
+            for i in Yhold:
+                X.append(Yhold[index])
+                Y.append(Xhold[index])
+                index += 1
+            Xhold.clear()
+            Yhold.clear()
+            index += 0
+        if deg == "180":
+            for i in X:
+                X[index] *= -1 
+                Y[index] *= -1
+                index += 1 
+                
+                
+            
+    elif Direction == "cc":
+        deg = input("Enter Rotation ('90','180','270')").lower()
+        if deg == "90":
+            for i in X:
+                X[index] *= -1
+                Xhold.append(X[index])
+                index+=1
+            X.clear()
+            index = 0
+            for i in Y:
+                Yhold.append(Y[index])
+                index += 1
+            Y.clear()
+            index = 0
+            for i in Yhold:
+                X.append(Yhold[index])
+                Y.append(Xhold[index])
+                index += 1
+            Xhold.clear()
+            Yhold.clear()
+            index += 0
+        if deg == "270":
+            for i in X:
+                X[index] *= -1 
+                Y[index] *= -1
+                index += 1 
+        index = 0 
+        
+        if deg == "90":
+            for i in X:
+                Xhold.append(X[index])
+                index+=1
+            X.clear()
+            index = 0
+            for i in Y:
+                Y[index] *= -1
+                Yhold.append(Y[index])
+                index += 1
+            Y.clear()
+            index = 0
+            for i in Yhold:
+                X.append(Yhold[index])
+                Y.append(Xhold[index])
+                index += 1
+            Xhold.clear()
+            Yhold.clear()
+            index += 0
+        if deg == "180":
+            for i in X:
+                X[index] *= -1 
+                Y[index] *= -1
+                index += 1 
+        
+    else:
+        print("Enter 'c' or 'cc'")
+    index = 0    
+    for item in X:
+        print(f'{index}({X[index]},{Y[index]})')
+        index += 1
+while True:      
+    rotate()
