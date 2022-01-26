a96c4ddc83fedd0dedaa32283ffc3651096e832b
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2022-01-26 11:04:13 -0500

    Pythagorean Theorem

--- Geomentry/Chapter 9 Right Triangles and Trigonometry/9.1 Pythagorean Theorem/Pythagorean_Theorem.py
+++ Geomentry/Chapter 9 Right Triangles and Trigonometry/9.1 Pythagorean Theorem/Pythagorean_Theorem.py
@@ -1,14 +1,14 @@
 import math
 def Pythagorean_theorem(leg,base,hyp):
     if hyp == 0:
-        return(f"Missing side is {math.sqrt(leg**2+base**2)}")
+        return(f"Missing side is {round(math.sqrt(leg**2+base**2),1)}")
     if leg == 0 or base == 0:
         if leg == 0 and base == 0:
             return "Invalid Triangle"
         if leg == 0:
-            return (f"Missing side is {math.sqrt((hyp**2)-(base**2))}")
+            return (f"Missing side is {round(math.sqrt((hyp**2)-(base**2)),1)}")
         else:
-            return (f"Missing side is {math.sqrt((hyp**2)-(leg**2))}")
+            return (f"Missing side is {round(math.sqrt((hyp**2)-(leg**2)),1)}")
     
 print("Enter Triangle Sides; Enter 0 if blank")
 
