640b75d3c88652be8afba2300f9b31d8ee2f6974
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2022-01-26 10:29:15 -0500

    Update Classifying_Triangles_By_Sides.py

--- Geomentry/Chapter 9 Right Triangles and Trigonometry/9.1 Pythagorean Theorem/Classifying_Triangles_By_Sides.py
+++ Geomentry/Chapter 9 Right Triangles and Trigonometry/9.1 Pythagorean Theorem/Classifying_Triangles_By_Sides.py
@@ -1,5 +1,7 @@
 def Verify_Triangle(leg,base,hyp):
-    if leg**2 + base**2 == hyp **2:
+    if leg + base < hyp:
+        return "Invalid Triangle"
+    elif leg**2 + base**2 == hyp **2:
         return "Right Triangle"
     elif hyp**2 < leg**2 + base **2:
         return "Acute Triangle"
