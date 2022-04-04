7c7afda8fde6aff747435f8ea77a73d6c86e5071
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2022-04-04 10:44:44 -0400

    Update ArcLength.py

--- Geomentry/Chapter 11 Circumference, Area, and Volume/Section 11.1 - Circumference and Arc Length/ArcLength.py
+++ Geomentry/Chapter 11 Circumference, Area, and Volume/Section 11.1 - Circumference and Arc Length/ArcLength.py
@@ -3,7 +3,7 @@ from fractions import Fraction
 def ArcLen(deg, rad):
     return(f'{Fraction((rad*2)*(deg/360)).limit_denominator()}π or {(rad*math.pi*2)*(deg/360)}')
 while True:
-    deg = input("Enter deg:")
-    rad = input("Enter rad:")
+    deg = int(input("Enter deg:"))
+    rad = int(input("Enter rad:"))
     print(ArcLen(deg,rad))
     print("")
